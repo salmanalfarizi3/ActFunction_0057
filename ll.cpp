de48c5c1f9b09e563dfb437786cd22ad5bc7ba50
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-03-02 20:56:17 +0700

    Menambahkan input

--- ll.cpp
+++ ll.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+float d1, d2;
+
+     void input  () {
+        cout << "Masukkan diagonal 1: ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2: ";
+        cin >> d2;
+    }
