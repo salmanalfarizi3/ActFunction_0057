970a40eee9443cac89f2a53850385237fb130555
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-03-02 21:03:35 +0700

    deklarasikan variabel dan implementasi fungsi dan prosedur

--- fungsi.cpp
+++ fungsi.cpp
@@ -2,3 +2,11 @@
 #include <iostream>
 using namespace std;
 //deklarasi variabel
+int p, l;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
